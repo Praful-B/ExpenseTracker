58ad2bccb5bcbce51c0a2969e1aaa011cd4b79ee
Praful-B <baleraoprafulchandra2006@gmail.com>
2026-02-19 02:11:33 +0530

    setup basic express backend

---
+++ expense-tracker-backend/src/server.ts
@@ -0,0 +1,21 @@
+import { Application, Request, Response } from "express";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT: number = Number(process.env.BACKEND_PORT) || 3000;
+
+app.get("/", (_req: Request, res: Response): void => {
+  res.status(200);
+  res.send("Hello, World from Backend");
+});
+
+app.listen(PORT, (error?: Error): void => {
+  if (!error) {
+    console.log(`Running Backend server on: ${PORT}`);
+  } else {
+    console.log("Error occurred, server can't start", error);
+  }
+});
